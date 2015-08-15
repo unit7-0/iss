21aa18f738665ff89782ed9884cf7d33dcb1126f
breezzo <vialurt@gmail.com>
2015-08-15 13:58:58 +0600

    some dao and tests

---
+++ src/main/java/com/unit7/iss/model/UserModel.java
@@ -0,0 +1,7 @@
+package com.unit7.iss.model;
+
+/**
+ * Created by breezzo on 09.08.15.
+ */
+public class UserModel {
+}
---
+++ src/test/java/com/unit7/iss/dao/UserModelParseTest.java
@@ -0,0 +1,7 @@
+package com.unit7.iss.dao;
+
+/**
+ * Created by breezzo on 09.08.15.
+ */
+public class UserModelParseTest {
+}
